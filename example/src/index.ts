d8a16db566b8d9e97c4ac6dd23cab7db99312fd1
Amine Rehioui <amine@franticsoftware.com>
2020-06-11 19:16:17 -0400

    Updated example app

--- example/src/index.ts
+++ example/src/index.ts
@@ -4,8 +4,15 @@ import { Chat } from "eterna-chat-wrapper";
 new Chat({
     container: document.getElementById('chat-container'),
     username: 'aminere-dev1',
-    uid: '260587',
+    uid: '260587',   
+    collapsible: true, 
     onHidden: () => {
         console.log('Chat.onHidden()')
+    },
+    onCollapsed: () => {
+        console.log('Chat.onCollapsed()');
+    },
+    onExpanded: () => {
+        console.log('Chat.onExpanded()');
     }
 });
